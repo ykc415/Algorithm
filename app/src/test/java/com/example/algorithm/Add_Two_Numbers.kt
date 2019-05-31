944e22677714d59ce1ef8572464df8f5e63db03f
ykc <pica415@gmail.com>
2019-05-31 16:16:22 +0900

    Initial commit

---
+++ app/src/test/java/com/example/algorithm/Add_Two_Numbers.kt
@@ -0,0 +1,23 @@
+package com.example.algorithm
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class ListNode(var `val`: Int) {
+     var next: ListNode? = null
+}
+
+class Solution {
+
+
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+
+        return null
+    }
+}
