a1b3bf27af047dcdca37baca38de8594220b870b
ykc <pica415@gmail.com>
2019-06-04 19:40:51 +0900

    3. Longest Substring Without Repeating Characters
    풀어버리고 말았다.

--- app/src/test/java/com/example/algorithm/longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.kt
+++ app/src/test/java/com/example/algorithm/longest_Substring_Without_Repeating_Characters/Longest_Substring_Without_Repeating_Characters.kt
@@ -1,8 +1,113 @@
 package com.example.algorithm.longest_Substring_Without_Repeating_Characters
 
-internal class Solution {
-    fun lengthOfLongestSubstring(s: String): Int {
+import org.junit.Test
 
-        return 0
+import org.junit.Assert.*
+
+
+/**
+ *  find the length of the longest substring without repeating characters.
+ *  반복되는 글자 없는 가장 긴 서브스트링
+ */
+
+internal class Solution {
+
+    fun lengthOfLongestSubstring(s: String): Int {
+        var length = 0
+
+        s.forEachIndexed { index, c ->
+            val set = linkedSetOf(c)
+
+            (index + 1 until s.length).map {
+                s[it]
+            }.takeWhile {
+                !set.contains(it).apply {
+                    set.add(it)
+                }
+            }
+                .toMutableList()
+                .let {
+                    it.add(0, c)
+                    it
+                }.takeIf {
+                    it.toSet().size == it.size
+                }.apply {
+                    if(length < this?.size ?: 0) {
+                        length = this?.size ?: 0
+                    }
+                }
+        }
+
+        return length
+    }
+
+
+    /**
+     *    abc'abc'bb
+     *    pw'wke'w
+     */
+    @Test
+    fun testCase1() {
+        val input = "abcabcbb"
+        val expected_length = 3
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase2() {
+        val input = "bbbbb"
+        val expected_length = 1
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase3() {
+        val input = "pwwkew"
+        val expected_length = 3
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase4() {
+        val input = " "
+        val expected_length = 1
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+
+    @Test
+    fun testCase5() {
+        val input = "c"
+        val expected_length = 1
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase6() {
+        val input = "aab"
+        val expected_length = 2
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase7() {
+        val input = "dvdf"
+        val expected_length = 3
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
+    }
+
+    @Test
+    fun testCase8() {
+        val input = "cdd"
+        val expected_length = 2
+
+        assertEquals(expected_length, lengthOfLongestSubstring(input))
     }
 }
